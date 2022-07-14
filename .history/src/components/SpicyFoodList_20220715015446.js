703381faaf24f62ef030bb74106c9cf701fe19cd
peterclermy <peterclermy232@gmail.com>
2022-07-15 01:57:01 +0300

    usestate

---
+++ .history/src/components/SpicyFoodList_20220715015446.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { spicyFoods, getNewRandomSpicyFood } from "../data";
+
+function SpicyFoodList() {
+  const [foods, setFoods] = useState(spicyFoods);
+
+  function handleAddFood() {
+    const newFood = getNewRandomSpicyFood();
+    console.log(newFood);
+  }
+
+  const foodList = foods.map((food) => (
+    <li key={food.id}>
+      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+    </li>
+  ));
+
+  return (
+    <div>
+      <button onClick={handleAddFood}>Add New Food</button>
+      <ul>{foodList}</ul>
+    </div>
+  );
+}
+
+export default SpicyFoodList;
